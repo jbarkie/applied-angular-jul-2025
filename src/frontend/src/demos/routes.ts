4d49c68b1b950c7d37453c3d27e7cb1df683e112
Joe Barkie <Joseph_Barkie@progressive.com>
2025-07-21 20:35:16 +0000

    Define routes for Demos

---
+++ src/frontend/src/demos/routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+import { Demos } from './demos';
+export const DEMOS_ROUTES: Routes = [
+  {
+    path: '',
+    component: Demos,
+    children: [
+      {
+        path: 'signals',
+        loadComponent: () =>
+          import('./pages/signals-demo').then((c) => c.SignalsDemo),
+      },
+    ],
+  },
+];
