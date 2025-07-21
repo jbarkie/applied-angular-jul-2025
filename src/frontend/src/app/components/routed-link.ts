9535360726d858cc7bc4c0a24318cc29f3551969
Joe Barkie <Joseph_Barkie@progressive.com>
2025-07-21 20:36:51 +0000

    Add RoutedLink component to reduce duplicate code and establish pattern

---
+++ src/frontend/src/app/components/routed-link.ts
@@ -0,0 +1,27 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  output,
+} from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-routed-link',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [RouterLink, RouterLinkActive],
+  template: ` <a
+    [routerLink]="href()"
+    routerLinkActive="underline"
+    #rla="routerLinkActive"
+    [class.opacity-60]="rla.isActive === false"
+    (click)="linkClicked.emit(label())"
+    >{{ label() }}</a
+  >`,
+  styles: ``,
+})
+export class RoutedLink {
+  href = input.required<string[]>();
+  label = input.required<string>();
+  linkClicked = output<string>();
+}
