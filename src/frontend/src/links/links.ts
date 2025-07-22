e256db6dfd66c58444fd6e8f80b2b84a44ec9c08
Joe Barkie <Joseph_Barkie@progressive.com>
2025-07-22 18:51:08 +0000

    Update component
    Add link to allow user to view Prefs component and update their preferences.

--- src/frontend/src/links/links.ts
+++ src/frontend/src/links/links.ts
@@ -1,11 +1,22 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { LinksStore } from './services/links-store';
 
 @Component({
   selector: 'app-links',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterOutlet],
-  template: ` <router-outlet /> `,
+  imports: [RouterOutlet, RouterLink],
+  providers: [LinksStore],
+  template: `
+    <div class="flex flex-row gap-4">
+      <a routerLink="/links" class="link">List</a>
+      <a routerLink="prefs" class="link">Prefs</a>
+    </div>
+
+    <div class="p-4">
+      <router-outlet />
+    </div>
+  `,
   styles: ``,
 })
 export class Links {}
