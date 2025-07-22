444a22d797b03b2bd260fcef3990db4ba4bd3169
Joe Barkie <Joseph_Barkie@progressive.com>
2025-07-22 16:30:10 +0000

    Add links handler to mock API call

---
+++ src/frontend/src/mocks/links-handler.ts
@@ -0,0 +1,35 @@
+import { http, delay, HttpResponse } from 'msw';
+
+const FAKE_LINKS = [
+  {
+    id: '1',
+    title: 'Angular Documentation',
+    url: 'https://angular.io/docs',
+    tags: ['angular', 'documentation'],
+    dateAdded: new Date('2023-01-01'),
+    owner: 'b145842',
+  },
+  {
+    id: '1',
+    title: 'React Documentation',
+    url: 'https://reactjs.org/docs',
+    tags: ['react', 'documentation'],
+    dateAdded: new Date('2024-06-30'),
+    owner: 'c732491',
+  },
+  {
+    id: '1',
+    title: 'Typescript Handbook',
+    url: 'https://www.typescriptlang.org/docs/handbook/intro.html',
+    tags: ['typescript', 'documentation'],
+    dateAdded: new Date('2023-03-15'),
+    owner: 'b875234',
+  },
+];
+
+export const links_handlers = [
+  http.get('https://links-api.fictionalcompany.com/api/links', async () => {
+    await delay(3000);
+    return HttpResponse.json(FAKE_LINKS);
+  }),
+];
