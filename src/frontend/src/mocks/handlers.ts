d264412203e20fba7845abd1173a12ba4f3898a5
Joe Barkie <Joseph_Barkie@progressive.com>
2025-07-23 18:10:13 +0000

    Add new identity handlers to global app handlers

--- src/frontend/src/mocks/handlers.ts
+++ src/frontend/src/mocks/handlers.ts
@@ -1,3 +1,4 @@
+import { identity_handlers } from './identity-handler';
 import { links_handlers } from './links-handler';
 
-export const handlers = [...links_handlers];
+export const handlers = [...links_handlers, ...identity_handlers];
