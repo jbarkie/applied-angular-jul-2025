a785122b6169b48e3604d2d1f45842ebe967fde2
Joe Barkie <Joseph_Barkie@progressive.com>
2025-07-23 17:54:52 +0000

    Add initial Edit component for editing links

---
+++ src/frontend/src/links/pages/edit.ts
@@ -0,0 +1,10 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-links-edit',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [],
+  template: ` <p>Edit a link</p> `,
+  styles: ``,
+})
+export class Edit {}
