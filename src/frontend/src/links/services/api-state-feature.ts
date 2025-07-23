81c9d320a6f7c1122f6441a974e95c79acb37181
Joe Barkie <Joseph_Barkie@progressive.com>
2025-07-23 15:33:23 +0000

    Add new functions to determine if loading and set state accordingly

--- src/frontend/src/links/services/api-state-feature.ts
+++ src/frontend/src/links/services/api-state-feature.ts
@@ -1,4 +1,5 @@
-import { signalStoreFeature, withState } from '@ngrx/signals';
+import { computed } from '@angular/core';
+import { signalStoreFeature, withComputed, withState } from '@ngrx/signals';
 
 type ApiState = {
   state: 'isLoading' | 'loading' | 'fetching' | 'idle';
@@ -9,5 +10,18 @@ export function withApiState() {
     withState<ApiState>({
       state: 'idle',
     }),
+    withComputed((store) => {
+      return {
+        isLoading: computed(() => store.state() === 'isLoading'),
+      };
+    }),
   );
+}
+
+export function setIsLoading(): ApiState {
+  return { state: 'isLoading' };
+}
+
+export function setIsFulfilled(): ApiState {
+  return { state: 'idle' };
 }
