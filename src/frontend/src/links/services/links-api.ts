98ef0d9d45decca921437cb83fbab50615bf9f34
Joe Barkie <Joseph_Barkie@progressive.com>
2025-07-23 15:20:56 +0000

    Add method that returns links array as a Signal

--- src/frontend/src/links/services/links-api.ts
+++ src/frontend/src/links/services/links-api.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { ApiLink } from '../types';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 export class LinksApiService {
   private baseUrl = 'https://links-api.fictionalcompany.com/api/links';
@@ -8,5 +9,9 @@ export class LinksApiService {
 
   getLinks() {
     return this.http.get<ApiLink[]>(this.baseUrl);
+  }
+
+  getLinksAsSignal() {
+    return toSignal(this.http.get<ApiLink[]>(this.baseUrl));
   }
 }
