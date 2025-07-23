be3da31cc4b76c7635581f01fd2b2e43dd0b281f
Joe Barkie <Joseph_Barkie@progressive.com>
2025-07-23 18:34:20 +0000

    Add action for login success

--- src/frontend/src/app/shared/identity/actions.ts
+++ src/frontend/src/app/shared/identity/actions.ts
@@ -1,8 +1,10 @@
-import { createActionGroup, emptyProps } from '@ngrx/store';
+import { createActionGroup, emptyProps, props } from '@ngrx/store';
+import { IdentityState } from './store';
 
 export const IdentityActions = createActionGroup({
   source: 'identity',
   events: {
     'Login Requested': emptyProps(),
+    'Login Succeeded': props<{ payload: IdentityState }>(),
   },
 });
