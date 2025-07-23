579442bb240d50b10c4c25bdb041feb0783d8ab3
Joe Barkie <Joseph_Barkie@progressive.com>
2025-07-23 18:21:35 +0000

    Provide global store and effects and provide Devtools for store

--- src/frontend/src/app/app.config.ts
+++ src/frontend/src/app/app.config.ts
@@ -6,7 +6,9 @@ import {
   withPreloading,
   withViewTransitions,
 } from '@angular/router';
-
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideEffects } from '@ngrx/effects';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
@@ -20,5 +22,8 @@ export const appConfig: ApplicationConfig = {
       withComponentInputBinding(),
     ),
     provideHttpClient(withFetch()),
+    provideStore(),
+    provideStoreDevtools(),
+    provideEffects([]),
   ],
 };
