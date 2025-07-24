a9c19c7a60d49d876d6618c3603126cb119bbbb4
Joe Barkie <Joseph_Barkie@progressive.com>
2025-07-24 16:18:47 +0000

    Add route guards to prefs and edit paths

--- src/frontend/src/links/links.routes.ts
+++ src/frontend/src/links/links.routes.ts
@@ -1,12 +1,19 @@
-import { Routes } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivateFn, Routes } from '@angular/router';
 import { Links } from './links';
 import { LinksList } from './pages/list';
 import { Prefs } from './pages/prefs';
 import { Edit } from './pages/edit';
+import { isLoggedInGuard } from '../app/shared/routing/logged-in-guard';
+import { LinksStore } from './services/links-store';
+import { inject } from '@angular/core';
+import { selectSub } from '../app/shared/identity/store';
+import { Store } from '@ngrx/store';
+import { LinksApiService } from './services/links-api';
 export const LINKS_ROUTES: Routes = [
   {
     path: '',
     component: Links,
+    providers: [LinksStore, LinksApiService],
     children: [
       {
         path: 'list',
@@ -15,11 +22,29 @@ export const LINKS_ROUTES: Routes = [
       {
         path: 'prefs',
         component: Prefs,
+        canActivate: [isLoggedInGuard],
       },
       {
         path: ':id/edit',
         component: Edit,
+        canActivate: [isLoggedInGuard, isOwnerOfLinkGuard],
       },
     ],
   },
 ];
+
+function isOwnerOfLinkGuard(): CanActivateFn {
+  return (route: ActivatedRouteSnapshot) => {
+    const store = inject(LinksStore);
+    const userSub = inject(Store).selectSignal(selectSub);
+    const linkId = route.paramMap.get('id');
+    if (linkId === null || userSub() === null) {
+      return false;
+    }
+    const link = store.entityMap()[linkId];
+    if (!link) {
+      return false;
+    }
+    return link.owner === userSub();
+  };
+}
