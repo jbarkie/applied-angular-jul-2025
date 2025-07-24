3231fe7e910cb778c30077cdc0f84e4e5049278a
Joe Barkie <Joseph_Barkie@progressive.com>
2025-07-24 16:19:29 +0000

    Provide services in links routes instead of component

--- src/frontend/src/links/links.ts
+++ src/frontend/src/links/links.ts
@@ -1,13 +1,10 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { LinksStore } from './services/links-store';
-import { LinksApiService } from './services/links-api';
 
 @Component({
   selector: 'app-links',
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [RouterOutlet, RouterLink],
-  providers: [LinksStore, LinksApiService],
   template: `
     <div class="flex flex-row gap-4">
       <a routerLink="list" class="link">List</a>
